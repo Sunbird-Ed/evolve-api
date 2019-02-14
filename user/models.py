5611df3d266ed7f053ccb7c27037777b519278bd
Anand Tiwary <anand.t@mantralabsglobal.com>
2019-02-14 13:33:14 +0530

    user table changed

--- user/models.py
+++ user/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import PermissionsMixin,AbstractUser
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 from apps.configuration.models import State
+from django.contrib.auth.models import User
+# class EvolveUser(AbstractUser):
+#     pass
 
-class EvolveUser(AbstractUser):
-    pass
+# class CustomUser(User):
+#     username_validator = ASCIIUsernameValidator()
     
 class Roles(models.Model):
     rolename=models.CharField(max_length=200)
@@ -16,6 +19,6 @@ class Roles(models.Model):
 class UserDetails(models.Model):
     state=models.ForeignKey(State,on_delete=models.CASCADE)
     role=models.ForeignKey(Roles,on_delete=models.CASCADE)
-    user=models.OneToOneField(EvolveUser,on_delete=models.CASCADE, to_field='id', primary_key=True)
+    user=models.OneToOneField(User,on_delete=models.CASCADE, to_field='id', primary_key=True)
 
 
