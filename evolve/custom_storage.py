f1bbba91b4b428903a5b1816c6bf1ea1b1996352
Anand Tiwary <anand.t@mantralabsglobal.com>
2019-02-15 16:18:40 +0530

    custom storage changed

--- evolve/custom_storage.py
+++ evolve/custom_storage.py
@@ -1,8 +1,11 @@
 from storages.backends.azure_storage import AzureStorage
-
+from evolve import settings
 class MediaStorage(AzureStorage):
     # location = 'media'
     # file_overwrite = True
+    account_name = settings.AZURE_ACCOUNT_NAME
+    account_key = settings.AZURE_ACCOUNT_KEY
+    azure_container = settings.AZURE_CONTAINER
     expiration_secs = None
 
 # class FileStorage(S3Boto3Storage):
