6974af1598f273810608118d532abf1fb2f4f919
Sukhjit Singh <sukhjit@mantralabsglobal.com>
2019-04-05 14:52:27 +0530

    filter with chapter id in hardspot

--- apps/hardspot/serializers.py
+++ apps/hardspot/serializers.py
@@ -306,9 +306,9 @@ class ApprovedHardSpotSerializer(serializers.ModelSerializer):
     def get_chapter(self, req):
         # import ipdb; ipdb.set_trace()
         data_str_list = []
-        chapters=Chapter.objects.filter(chapter=req.chapter).first()
+        chapters=Chapter.objects.filter(id=req.id).first()
         tempList = [ chapters.book.subject.grade.medium.state, chapters.book.subject.grade.medium, chapters.book.subject.grade, chapters.book.subject, chapters.book, chapters.chapter ]
-        chapter_hardspot = HardSpot.objects.filter(chapter__id=chapters.id,approved=True)
+        chapter_hardspot = HardSpot.    .filter(chapter__id=chapters.id,approved=True)
         section = ""
         sub_section = ""
         sub_sub_section= ""
