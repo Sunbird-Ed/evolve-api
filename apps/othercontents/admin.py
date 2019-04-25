91760291a6a303663b1c5eab2bf9667cc9cb7316
Sukhjit Singh <sukhjit@mantralabsglobal.com>
2019-04-25 12:58:19 +0530

    remove school table

--- apps/othercontents/admin.py
+++ apps/othercontents/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Tags,OtherContributors,OtherContent,SchoolName
+from .models import Tags,OtherContributors,OtherContent
 
 
 admin.site.register(Tags)
 admin.site.register(OtherContributors)
 admin.site.register(OtherContent)
-admin.site.register(SchoolName)
+# admin.site.register(SchoolName)
