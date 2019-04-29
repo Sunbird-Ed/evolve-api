92a3a9c38bb111a8777d16562e006041cd185d72
Sukhjit Singh <sukhjit@mantralabsglobal.com>
2019-04-29 15:27:23 +0530

    remove save method from other content

--- apps/othercontents/models.py
+++ apps/othercontents/models.py
@@ -91,13 +91,13 @@ class OtherContent(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-    # def save(self, *args, **kwargs):
-    #     if self.file_url == None and self.text == "":
-    #         raise ValueError("document url and text ,both null values are Not allowed")
-    #     elif self.file_url != None and self.text != "":
-    #         raise ValueError("document url and text ,both values are Not allowed")
-    #     else:
-    #         super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if self.file_url == None and self.text == None:
+            raise ValueError("document url and text ,both null values are Not allowed")
+        elif self.file_url != None and self.text != None:
+            raise ValueError("document url and text ,both values are Not allowed")
+        else:
+            super().save(*args, **kwargs)
 
      
  
