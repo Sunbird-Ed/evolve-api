d5929d4ccb61ebc64e78c66d830f3c0f000e9d2e
Sukhjit Singh <sukhjit@mantralabsglobal.com>
2019-05-23 14:04:33 +0530

    change content admin

--- apps/content/admin.py
+++ apps/content/admin.py
@@ -3,6 +3,34 @@ from django.contrib import admin
 # Register your models here.
 from .models import Content,ContentContributors
 
-admin.site.register(Content)
+
+
+class ContentAdmin(admin.ModelAdmin):
+    model = Content
+    list_display = ['content_name','chapter_state','section_state','subsection_state','subsubsection_state']
+
+    def chapter_state(self,req):
+        try:
+            return req.chapter.book.subject.grade.medium.state
+        except:
+            return None
+    def section_state(self,req):
+        try:
+            return req.section.chapter.book.subject.grade.medium.state
+        except:
+            return None
+    def subsection_state(self,req):
+        try:
+            return req.sub_sub_section.section.chapter.book.subject.grade.medium.state
+        except:
+            return None
+
+    def subsubsection_state(self,req):
+        try:
+            return req.sub_sub_section.sub_sub_section.section.chapter.book.subject.grade.medium.state
+        except:
+            return None
+
 admin.site.register(ContentContributors)
+admin.site.register(Content,ContentAdmin)
 
