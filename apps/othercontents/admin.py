251afd410ef1929f48c3d8dbc1de59ab0e63ea36
Sukhjit Singh <sukhjit@mantralabsglobal.com>
2019-05-29 13:18:04 +0530

    change othercontent admin

--- apps/othercontents/admin.py
+++ apps/othercontents/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from .models import Tags,OtherContributors,OtherContent,SchoolName
 
 
+
+
+
+class SchoolNames(admin.ModelAdmin):
+    model = SchoolName
+    list_display = ['id','school_name']
+
 admin.site.register(Tags)
 admin.site.register(OtherContributors)
 admin.site.register(OtherContent)
-admin.site.register(SchoolName)
+admin.site.register(SchoolName,SchoolNames)
